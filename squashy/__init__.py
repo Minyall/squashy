8f040d11495a1f5f183cb70d72547116b3d43df8
James Allen-Robertson <minyall@gmail.com>
2023-05-04 11:44:10 +0100

    Finalise basic data loader

--- squashy/__init__.py
+++ squashy/__init__.py
@@ -1,3 +1,4 @@
 from squashy.agglomeration import GraphAgglomerator, MetaRelate
 from squashy.decomposition import KCoreIdentifier
 from squashy.squash import Squash
+from squashy.io import DataImporter
--- squashy/io.py
+++ squashy/io.py
@@ -1,66 +1,90 @@
-from mini_memgraph import Memgraph
 from typing import List, Tuple
 
+from mini_memgraph import Memgraph
+
+
 class DataImporter:
-    def __init__(self, database: Memgraph = None, node_label='NODE', edge_label='REL', weight_label: str = 'weight', address: str = 'locahost', port: int = 7687):
+    node_label: str
+    edge_label: str
+
+    def __init__(self, database: Memgraph = None, node_label='NODE', edge_label='REL', weight_label: str = None,
+                 address: str = 'locahost', port: int = 7687, wipe_db: bool = False):
         if database is None:
             self.db = Memgraph(address, port)
         else:
             self.db = database
-        self.set_node_label(node_label)
-        self.set_edge_label(edge_label)
+
+        if not self.db_is_empty():
+            if not wipe_db:
+                raise Exception(f'Your Memgraph instance already has data in it. Pass wipe_db=True to clear. Warning this will destroy all existing data in your Memgraph instance at {address=} {port=}')
+            else:
+                self.clear_database()
+        self.node_label = self.check_label(node_label)
+        self.edge_label = self.check_label(edge_label)
         self.weight_label = weight_label
 
+        self.db.set_index(node_label)
+        self.db.set_index(node_label, 'id')
+        self.db.set_constraint(node_label, 'id')
+
+    def db_is_empty(self):
+        res = self.db.read('MATCH (n) RETURN n LIMIT 1')
+        return len(res) == 0
+
+    def clear_database(self):
+        self.db.write('MATCH (n) DETACH DELETE n')
+
+
     def _prep_node_list(self, nodes: List):
-        return [{id:n} for n in nodes]
+        return [{'id': n} for n in nodes]
 
     def _prep_edge_list(self, edges: List):
         if self.weight_label is not None:
             prepped = [{'source': record[0],
-                 'target':record[1],
-                 self.weight_label:record[2]} for record in edges]
+                        'target': record[1],
+                        self.weight_label: record[2]} for record in edges]
         else:
             prepped = [{'source': record[0],
-                 'target':record[1]} for record in edges]
+                        'target': record[1]} for record in edges]
+        return prepped
 
     def load_nodes(self, node_list: List):
+        if not isinstance(node_list, list):
+            raise TypeError('node_list must be a list of node ids')
+        if len(node_list) > len(set(node_list)):
+            raise ValueError('All ids in the node list must be unique')
         data = self._prep_node_list(node_list)
         self.db.write_nodes(data, label=self.node_label, id_val='id')
 
-    def load_edges(self, edge_list: List[Tuple], weight_label:):
-        if not isinstance(edge_list,list) or not isinstance(edge_list[0], tuple) or not len(edge_list[0]) > 1:
+    def load_edges(self, edge_list: List[Tuple]):
+        if not isinstance(edge_list, list) or not isinstance(edge_list[0], tuple) or not len(edge_list[0]) > 1:
             raise TypeError('edge_list must be a list of Tuples of len 2 or 3 if including weight')
         data = self._prep_edge_list(edge_list)
-        self.db.write_edges(data, label=self.node_label, edge_label=self.edge_label, on_duplicate_edges='increment', add_attributes=[self.weight_label], id_val='id')
+        self.db.write_edges(data, source_label=self.node_label, edge_label=self.edge_label,
+                            target_label=self.node_label,
+                            on_duplicate_edges='increment', add_attributes=[self.weight_label],
+                            source_id_label='id', target_id_label='id')
 
-    def _set_label(self, label:str, attr:str):
-        if not label.isupper()
+    def check_label(self, label: str):
+        if not label.isupper():
             raise Exception(f'Node or edge labels should be UPPERCASE. {label=}')
-        setattr(self,attr,label)
+        return label
 
-    def set_node_label(self, label:str):
+    def set_node_label(self, label: str):
         self._set_label(label, '_node_label')
 
-    def set_edge_label(self, label: str):
-        self._set_label(label, '_edge_label')
 
-    def set_node_label(self, label:str):
-        self._set_label(label, '_edge_label')
-
-    def load_from_edge_list(self, edge_tuples: List[Tuple], node_label, edge_label, weight_label):
+    def load_from_edge_list(self, edge_tuples: List[Tuple]):
         node_list = set()
         node_list.update([record[0] for record in edge_tuples])
         node_list.update([record[1] for record in edge_tuples])
         node_list = list(node_list)
-        node_data = self._prep_node_list(node_list)
+        self.load_nodes(node_list)
+        self.load_edges(edge_tuples)
 
-        self.db.write_nodes(node_list=)
-
-    @property
-    def node_label(self):
-        return self._node_label
-
-    @property
-    def edge_label(self):
-        return self._edge_label
+    def report(self):
+        n_nodes = self.db.node_count(self.node_label)
+        n_rels = self.db.read(f'MATCH ()-[r:{self.edge_label}]-() WITH DISTINCT r RETURN count(r) AS n_rels')[0][
+            'n_rels']
+        return f"Database currently has {n_nodes:,} {self.node_label} nodes, and {n_rels:,} {self.edge_label} edges."
 
